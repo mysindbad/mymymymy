e8e73256f0f9d2407441130398c043ccbf7cfbae
mysindbad <mysindbad.traveler.ai@gmail.com>
2026-09-12 23:59:09 +0100

    Fix Vercel ESM type imports

--- server/dal.ts
+++ server/dal.ts
@@ -1,4 +1,4 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient, type SupabaseClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
